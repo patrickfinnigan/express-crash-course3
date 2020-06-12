3137aa57a5decf895ac627ecb22b8db413ad009b
patrickfinnigan <patrickfinnigan94@gmail.com>
2020-06-12 08:15:16 -0700

    practice makes perfect

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: "auitsu",
+        email: "test@yahoo.com",
+        status: "active"
+    },
+    {
+        id: 2,
+        name: "doitsu",
+        email: "name@gmail.com",
+        status: "active"
+    },
+    {
+        id: 3,
+        name: "koitsu",
+        email: "test@yahoo.com",
+        status: "inactive"
+    },
+];
+
+module.exports = members
